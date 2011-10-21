c8e308f5a75257514790ab0c27f45f881358a2d2
ryan.braganza@gmail.com <ryan.braganza@gmail.com@1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e>
2011-10-21 04:58:12 +0000

    tidyup (mecat-ansto/embargo-ryan)
    git-svn-id: https://mytardis.googlecode.com/svn/apps/mecat-ansto/branches/embargo-ryan@2096 1bbb43ac-ffe0-11de-8c0c-f1c8ac664a8e

--- mecat/templatetags/embargo.py
+++ mecat/templatetags/embargo.py
@@ -4,8 +4,8 @@ from mecat.embargo import EmbargoHandler
 
 register = template.Library()
 
-@register.inclusion_tag('inclusion_tags/expiry_editing.html', takes_context=True)
-def embargo_edit(context, experiment_id):
+@register.inclusion_tag('inclusion_tags/expiry_editing.html')
+def embargo_edit(experiment_id):
     handler = EmbargoHandler(experiment_id)
     inclusion_context = {'experiment_id': experiment_id}
     if handler.never_expires():
@@ -13,7 +13,6 @@ def embargo_edit(context, experiment_id):
         inclusion_context['because_no_end_date'] = handler.because_no_end_date()
     elif handler.has_any_expiry():
         inclusion_context['has_any_expiry'] = True
-        import datetime
         inclusion_context['expiry_date'] = handler.get_expiry_date().strftime('%Y/%m/%d')
     else:
         raise Exception('unknown state - should never get here')
